153e5a00175728704cf524ea6c1e530d049626d3
Toktam <ttmohebbi@gmail.com>
2021-04-21 00:24:22 +0000

    setup the Tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+  {
+    "id": 1,
+    "text": "Doctors Appointment",
+    "day": "May 5th at 2:30pm",
+    "reminder": true
+  },
+  {
+    "id": 2,
+    "text": "Standup",
+    "day": "May 6th at 1:30pm",
+    "reminder": true
+  },
+  {
+    "id": 3,
+    "text": "Grocery Shopping",
+    "day": "May 6th at 1:30pm",
+    "reminder": false
+  }
+]
+
+const Tasks = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Tasks;
