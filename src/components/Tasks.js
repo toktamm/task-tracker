939f6e98e6dbaec9e0ce37f366f5634790785b93
Toktam <ttmohebbi@gmail.com>
2021-04-21 00:35:14 +0000

    set tasks as a state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,30 @@
-const tasks = [
-  {
-    "id": 1,
-    "text": "Doctors Appointment",
-    "day": "May 5th at 2:30pm",
-    "reminder": true
-  },
-  {
-    "id": 2,
-    "text": "Standup Meeting",
-    "day": "May 6th at 1:30pm",
-    "reminder": true
-  },
-  {
-    "id": 3,
-    "text": "Grocery Shopping",
-    "day": "May 6th at 1:30pm",
-    "reminder": false
-  }
-]
+import { useState } from "react";
 
 const Tasks = () => {
+
+  const [tasks, setTasks] = useState(
+    [
+      {
+        "id": 1,
+        "text": "Doctors Appointment",
+        "day": "May 5th at 2:30pm",
+        "reminder": true
+      },
+      {
+        "id": 2,
+        "text": "Standup Meeting",
+        "day": "May 6th at 1:30pm",
+        "reminder": true
+      },
+      {
+        "id": 3,
+        "text": "Grocery Shopping",
+        "day": "May 6th at 1:30pm",
+        "reminder": false
+      }
+    ]
+  )
+
   return (
     <>
 
