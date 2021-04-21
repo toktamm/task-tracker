d25020994c57813cd61493153ed9a00d370f3d49
Toktam <ttmohebbi@gmail.com>
2021-04-21 02:08:23 +0000

    create delete task option

--- src/App.js
+++ src/App.js
@@ -29,11 +29,18 @@ function App() {
     ]
   )
 
+// Delete Task
+const deleteTask = (id) => {
+setTasks(tasks.filter((task) => task.id !== id ))
+}
+
+
+
   return (
     
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
 
   );
